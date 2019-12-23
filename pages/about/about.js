1a4c94c0348b8d4954b6163767f2ee7c36924c41
wangguocheng <1284374599@qq.com>
2019-12-23 16:32:07 +0800

    内纸组件

--- pages/about/about.js
+++ pages/about/about.js
@@ -5,62 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    message: '12',
+    imgSrc: ''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  chooseImg() {
+    // console.log('this')
+    wx.chooseImage({
+      success: (res) => {
+        this.setData({
+          imgSrc: res.tempFilePaths[0]
+        })
+      },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  imgLoadedEvent() {
+    // console.log(123)
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -45,23 +45,44 @@ Page({
     // console.log(app.globalData)
   },
 
+  avatorUrlClicked: () => {
+    console.log("头像被点击了")
+  },
+
+  // 监听页面滚动
+  onPageScroll(obj) {
+    // console.log(obj)
+  },
+  // 监听页面滚动到底部
+  onReachBottom() {
+    console.log('滚动到底部')
+    console.log(123)
+  },
+  onPullDownRefresh() {
+    console.log('下拉刷新')
+    console.log(123)
+  },
+
   // 页面的生命周期函数
   onLoad(options) {
     let Timestamp = Math.round(new Date() / 1000)
     let Nonce = Math.floor((Math.random() * 10) + 1) * Timestamp
     let Signature = ''
     wx.getUserInfo({
-      success: function(res) {
-        console.log(res)
-        Signature = res.signature
+      success: (res) => {
+        // console.log(res)
+        // Signature = res.signature
+        this.setData({
+          counter: res.userInfo.gender
+        })
       }
     })
-    wx.request({
-      url: `https://iai.tencentcloudapi.com/?Action=CreateGroup&GroupName=计算机11602&FaceModelVersion=3.0&GroupId=jisuanji11602&Version=2018-03-01&Timestamp=${Timestamp}&Nonce=${Nonce}&SecretId=AKIDD1auyzXUu47wVpdqVv52lFVZS6UiFX4K&Signature=${Signature}`,
-      success(res) {
-        console.log(res)
-      }
-    })
+    // wx.request({
+    //   url: `https://iai.tencentcloudapi.com/?Action=CreateGroup&GroupName=计算机11602&FaceModelVersion=3.0&GroupId=jisuanji11602&Version=2018-03-01&Timestamp=${Timestamp}&Nonce=${Nonce}&SecretId=AKIDD1auyzXUu47wVpdqVv52lFVZS6UiFX4K&Signature=${Signature}`,
+    //   success(res) {
+    //     console.log(res)
+    //   }
+    // })
   },
   onReady() {
 
@@ -76,19 +97,6 @@ Page({
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
 
   /**
    * 用户点击右上角分享
